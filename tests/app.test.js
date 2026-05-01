ec1b09140d4cce00603ce8fbd877ea199c71b058
Nonny <nonnyeleweke@gmail.com>
2026-05-01 08:22:13 +0100

    Fixed EC2 secrets

--- tests/app.test.js
+++ tests/app.test.js
@@ -7,7 +7,6 @@ describe('GET /', () => {
     expect(response.statusCode).toBe(200);
   });
   
-
   it('should return Hello World', async () => {
     const response = await request(app).get('/');
     expect(response.text).toBe('Hello World');
